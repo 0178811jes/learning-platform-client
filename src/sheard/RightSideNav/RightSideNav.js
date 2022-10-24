be39beb485b423a1268ef234416f0b02940c286d
0178811jes <jesminara8653@gmail.com>
2022-10-25 02:59:21 +0600

    header set

--- src/sheard/RightSideNav/RightSideNav.js
+++ src/sheard/RightSideNav/RightSideNav.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 
 const RightSideNav = () => {
     return (
-        <div>
-            <h6>This is right side</h6>
-        </div>
+        <ButtonGroup vertical>
+            <Button variant="outline-primary">Button</Button>
+            <Button variant="outline-dark">Button</Button>
+        </ButtonGroup>
     );
 };
 

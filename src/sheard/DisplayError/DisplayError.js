e8112b0ad51485059eb7a848b4d959c7ef7378de
0178811jes <jesminara8653@gmail.com>
2022-12-02 08:02:22 +0600

    some changes

--- src/sheard/DisplayError/DisplayError.js
+++ src/sheard/DisplayError/DisplayError.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import {  useNavigate, useRouteError } from 'react-router-dom';
+import {  useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 
 const DisplayError = () => {
-    const error = useRouteError();
+   
     const {logout} = useContext(AuthContext)
     const navigate = useNavigate();
 
@@ -19,7 +19,7 @@ const DisplayError = () => {
     return (
         <div>
             <p>Something went wrong</p>
-            <p>{error.statusText || error.message}</p>
+            
             <h4>Please <button onClick={handleLogout}>Sign Out</button> and log back in</h4>
         </div>
     );
